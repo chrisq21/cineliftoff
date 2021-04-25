0baf9c32c3dc58f419f4cb60f1fbea4c5a9e45e1
Chris Queen <chrisqueen10@gmail.com>
2021-04-25 13:40:26 -0400

    test update

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Gatsby Starter Blog`,
     author: {
       name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      summary: `who lives and works in San Francisco.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
